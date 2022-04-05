352a145a9a0f07a9de408705ec4f0c18d59cdef9
Your Namels <selina.kirk82@gmail.com>
2022-04-05 18:23:06 +0100

    update

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-console.log("My script is working")
+// create an array of letters
+var ucChars = ['A', 'B', 'C'];
+var lcChars = ['a', 'b', 'c'];
+var numberChars = ['1', '2', '3'];
+var specialChars = ['%', '&', '^'];
+
+
+var hasUpper = true;
+var hasLower = false;
+var hasNumber = true;
+var hasSpecial = true;
+var passwordLength = 12;
+
+// create a master array of possible characters
+
+if (hasLower) {
+    masterArray.concat(lcChars);
+}
+
+if (hasUpper) {
+    masterArray.concat(ucChars);
+}
+
+if (hasUpper) {
+    masterArray.concat(numberChars);
+}
+
+if (hasUpper) {
+    masterArray.concat(specialChars);
+}
+
+var newPassword = "";
+
+for (var i = 0; i < passwordLength; i++) {
+    var randomIndex = Math.floor(Math.random() * masterArray.length)
+    newPassword += masterArray[randomIndex]
+}
