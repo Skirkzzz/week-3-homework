57f062784de218285bd6a93d7e8a311832371039
Your Namels <selina.kirk82@gmail.com>
2022-05-20 21:48:28 +0100

    commit

--- script.js
+++ script.js
@@ -2,11 +2,10 @@
 var generateBtn = document.querySelector("#generate");
 
 // create an array of letters
-var ucChars = ['A', 'B', 'C'];
-var lcChars = ['a', 'b', 'c'];
-var numberChars = ['1', '2', '3'];
-var specialChars = ['%', '&', '^'];
-
+var ucChars = ["A", "B", "C"];
+var lcChars = ["a", "b", "c"];
+var numberChars = ["1", "2", "3"];
+var specialChars = ["%", "&", "^"];
 
 var hasUpper = true;
 var hasLower = false;
@@ -17,36 +16,34 @@ var passwordLength = 12;
 // create a master array of possible characters
 
 if (hasLower) {
-    masterArray.concat(lcChars);
+  masterArray.concat(lcChars);
 }
 
 if (hasUpper) {
-    masterArray.concat(ucChars);
+  masterArray.concat(ucChars);
 }
 
 if (hasUpper) {
-    masterArray.concat(numberChars);
+  masterArray.concat(numberChars);
 }
 
 if (hasUpper) {
-    masterArray.concat(specialChars);
-
+  masterArray.concat(specialChars);
 }
 
 // Write password to the #password input
 function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-    passwordText.value = password;
-
+  passwordText.value = password;
 }
 
 var newPassword = "";
 
 for (var i = 0; i < passwordLength; i++) {
-    var randomIndex = Math.floor(Math.random() * masterArray.length)
-    newPassword += masterArray[randomIndex]
+  var randomIndex = Math.floor(Math.random() * masterArray.length);
+  newPassword += masterArray[randomIndex];
 }
 
 // Add event listener to generate button
